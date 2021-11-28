086be28cf6a3f9334b6b05d8e507862d4a963e92
XavaLab <jpastensalinas@gmail.com>
2021-11-28 18:46:20 -0300

    Estableciendo página de recuperar password

--- welcome.php
+++ welcome.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>&#129386 Bienvenido a Comida Facilita</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
@@ -21,14 +21,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
     <div class="page-header">
-        <h1>Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenid@ a nuestro sitio.</h1>
+        <h1>Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenid@ a Comida Facilita.</h1>
         
     </div>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Cambia tu contraseña</a>
         <a href="logout.php" class="btn btn-danger">Cierra la sesión</a>
-        <br><br><h2>Clic en el logo para más tips <a href="https://www.configuroweb.com/desarrollo/"><img src="images/configuroweb.png" width="170"></a>
-        <br><br><iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLEp4kajrb4a0fgy8e5JO3iGO7iKnPwQFQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      
     </p>
 
 
